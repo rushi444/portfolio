b7798cd572f4bfce4647ddd4fe8c85fd66f146d7
rushi <rushi44@gmail.com>
2021-10-17 21:46:39 -0700

    home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Main } from '../layout/Main'
+import { theme } from '../lib/theme'
+import { Fonts } from '../layout/fonts'
+
+const MyApp = ({ Component, pageProps, router }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Main router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Main>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
